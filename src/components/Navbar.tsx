cb55c4f652c6164e98b4531ca98529a7f41e48cb
anohene1 <anoheneisaac@gmail.com>
2024-01-19 11:10:00 +0000

    decrease padding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar() {
     return (
-      <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 md:py-8 2xl:px-0 w-full">
+      <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 2xl:px-0 w-full">
         <img src="/duelx-logo.png" alt="duelx logo" className="w-16" />
         <a className="rounded-full border border-white p-4 text-sm transition hover:bg-gray hover:bg-opacity-20 md:p-5 md:text-base">
           Read Whitepaper
