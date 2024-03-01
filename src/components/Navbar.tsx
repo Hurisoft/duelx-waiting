57182b160a1bcebd27454e66ce5bd18ec0cf8010
anohene1 <anoheneisaac@gmail.com>
2024-03-01 12:18:39 +0000

    show account

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useAccountModal} from "@rainbow-me/rainbowkit";
 
 function Navbar() {
+    const { openAccountModal } = useAccountModal();
+
     return (
       <nav className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 2xl:px-0 w-full">
         <img src="/duelx-logo.png" alt="duelx logo" className="w-16" />
-        <a className="rounded-full border border-white p-4 text-sm transition hover:bg-gray hover:bg-opacity-20 md:p-5 md:text-base">
+        <a onClick={openAccountModal} className="rounded-full border border-white p-4 text-sm transition hover:bg-gray hover:bg-opacity-20 md:p-5 md:text-base">
           Read Whitepaper
         </a>
       </nav>
